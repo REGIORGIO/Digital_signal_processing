d081b2ab9e1de50dfaaf3caa0e75994be3e17940
Georgy Timoshenskiy <timoshenskij.gd@edu.spbstu.ru>
2020-04-18 18:49:39 +0300

    Add Haralick generator

--- classificatorGLCM_NN.py
+++ classificatorGLCM_NN.py
@@ -13,131 +13,143 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
 import keras
 import keras.utils
 from keras import utils as np_utils
-
-
-def image_show(image, nrows=1, ncols=1, cmap='gray'):
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(14, 14))
-    ax.imshow(image, cmap='gray')
-    ax.axis('off')
-    return fig, ax
-
+import sklearn.metrics as metrics
+import random
 
 def generator_data(root):
-    global image_index
-    global illness_index
-    global df_data
-    global df_res
+    image_index = 0
+    illness_index = 1
+
+    df_data = []  # saving parameters Haralick's
+    
     for i, folder_dir in enumerate(os.listdir(root)):
         if folder_dir[0] == ".":
             continue
 
         print("Folder: ", folder_dir)
-        img_RED_global = []
-        img_GREEN_global = []
-        img_BLUE_global = []
         for _, filename in enumerate(os.listdir(root + folder_dir)):
             path = root + folder_dir + "/" + filename
             if filename[0] == ".":
                 continue
-            print(" -Filename: ", filename)
-            img_new = skimage.io.imread(path) #Загружаем очередное изображение
+            img_new = skimage.io.imread(path) #Загружаем очередное изображение по указанному пути
+            print(" -Filename: {}, Original Image Shape:{} ".format(filename, img_new.shape)) #выводим разрешение изображения и цвета(?)
+            # print(img_new)
 
             # red
             img_red = img_new[:, :, 0]
+            # print(img_red)
+            # print(img_red.shape)
+            # print(print(" -Filename: {}, RED Image Shape:{} ".format(filename, img_red.shape)))
             img_RED_global = img_red
             img_red = np.true_divide(img_red, 32)
             img_red = img_red.astype(int)
             glcm = greycomatrix(img_red, [1], [0], levels=8, symmetric=False, normed=True)
 
-            df_data.loc[image_index] = [greycoprops(glcm, 'correlation')[0, 0],
-                                        greycoprops(glcm, 'contrast')[0, 0],
-                                        greycoprops(glcm, 'homogeneity')[0, 0],
-                                        greycoprops(glcm, 'energy')[0, 0]]
-            df_res.loc[image_index] = dummy_y[int(folder_dir) - 1]
+            red_correlation = greycoprops(glcm, 'correlation')[0, 0]
+            red_contrast =  greycoprops(glcm, 'contrast')[0, 0]
+            red_homogeneity = greycoprops(glcm, 'homogeneity')[0, 0]
+            red_energy = greycoprops(glcm, 'energy')[0, 0]
+
             image_index += 1
+            #
             #  green
-            img_green = img_new[:, :, 2]
+            img_green = img_new[:, :, 1]
+            # print(print(" -Filename: {}, GREEN Image Shape:{} ".format(filename, img_green.shape)))
             img_GREEN_global = img_green
             img_green = np.true_divide(img_green, 32)
             img_green = img_green.astype(int)
             glcm = greycomatrix(img_green, [1], [0], levels=8, symmetric=False, normed=True)
 
-            df_data.loc[image_index] = [greycoprops(glcm, 'correlation')[0, 0],
-                                        greycoprops(glcm, 'contrast')[0, 0],
-                                        greycoprops(glcm, 'homogeneity')[0, 0],
-                                        greycoprops(glcm, 'energy')[0, 0]]
-            df_res.loc[image_index] = dummy_y[int(folder_dir) - 1]
+            green_correlation = greycoprops(glcm, 'correlation')[0, 0]
+            green_contrast = greycoprops(glcm, 'contrast')[0, 0]
+            green_homogeneity = greycoprops(glcm, 'homogeneity')[0, 0]
+            green_energy = greycoprops(glcm, 'energy')[0, 0]
             image_index += 1
+            #
             # blue
-            img_blue = img_new[:, :, 0]
+            img_blue = img_new[:, :, 2]
+            # print(print(" -Filename: {}, BLUE Image Shape:{} ".format(filename, img_blue.shape)))
             img_BLUE_global = img_blue
             img_blue = np.true_divide(img_blue, 32)
             img_blue = img_blue.astype(int)
             glcm = greycomatrix(img_blue, [1], [0], levels=8, symmetric=False, normed=True)
 
-            df_data.loc[image_index] = [greycoprops(glcm, 'correlation')[0, 0],
-                                        greycoprops(glcm, 'contrast')[0, 0],
-                                        greycoprops(glcm, 'homogeneity')[0, 0],
-                                        greycoprops(glcm, 'energy')[0, 0]]
-            df_res.loc[image_index] = dummy_y[int(folder_dir) - 1]
+            blue_correlation = greycoprops(glcm, 'correlation')[0, 0]
+            blue_contrast = greycoprops(glcm, 'contrast')[0, 0]
+            blue_homogeneity = greycoprops(glcm, 'homogeneity')[0, 0]
+            blue_energy = greycoprops(glcm, 'energy')[0, 0]
+
+            df_data.append({'red_correlation': red_correlation,
+                            'red_contrast': red_contrast,
+                            'red_homogeneity': red_homogeneity,
+                            'red_energy': red_energy,
+                            'green_correlation': green_correlation,
+                            'green_contrast': green_contrast,
+                            'green_homogeneity': green_homogeneity,
+                            'green_energy': green_energy,
+                            'blue_correlation': blue_correlation,
+                            'blue_contrast': blue_contrast,
+                            'blue_homogeneity': blue_homogeneity,
+                            'blue_energy': blue_energy,
+                            'class': int(folder_dir)})
+
             image_index += 1
-            # red-green
-            img_r_g = img_RED_global - img_GREEN_global
-            img_r_g = np.true_divide(img_r_g, 32)
-            img_r_g = img_r_g.astype(int)
-            glcm = greycomatrix(img_r_g, [1], [0], levels=8, symmetric=False, normed=True)
-
-            df_data.loc[image_index] = [greycoprops(glcm, 'correlation')[0, 0],
-                                        greycoprops(glcm, 'contrast')[0, 0],
-                                        greycoprops(glcm, 'homogeneity')[0, 0],
-                                        greycoprops(glcm, 'energy')[0, 0]]
-            df_res.loc[image_index] = dummy_y[int(folder_dir) - 1]
-            image_index += 1
-
-            #  red-blue
-            img_r_b = img_RED_global - img_BLUE_global
-            img_r_b = np.true_divide(img_r_b, 32)
-            img_r_b = img_r_b.astype(int)
-            glcm = greycomatrix(img_r_b, [1], [0], levels=8, symmetric=False, normed=True)
-
-            df_data.loc[image_index] = [greycoprops(glcm, 'correlation')[0, 0],
-                                        greycoprops(glcm, 'contrast')[0, 0],
-                                        greycoprops(glcm, 'homogeneity')[0, 0],
-                                        greycoprops(glcm, 'energy')[0, 0]]
-            df_res.loc[image_index] = dummy_y[int(folder_dir) - 1]
-            image_index += 1
-            # green-blue
-            img_g_b = img_GREEN_global - img_BLUE_global
-            img_g_b = np.true_divide(img_g_b, 32)
-            img_g_b = img_g_b.astype(int)
-            glcm = greycomatrix(img_g_b, [1], [0], levels=8, symmetric=False, normed=True)
-
-            df_data.loc[image_index] = [greycoprops(glcm, 'correlation')[0, 0],
-                                        greycoprops(glcm, 'contrast')[0, 0],
-                                        greycoprops(glcm, 'homogeneity')[0, 0],
-                                        greycoprops(glcm, 'energy')[0, 0]]
-            df_res.loc[image_index] = dummy_y[int(folder_dir) - 1]
-            image_index += 1
-
         illness_index += 1
+    return df_data
+
+
+def generate_haralick_params(df_data):
+    df_data_new = []
+    for i in range(1, len(df_data) - 1):
+        if df_data[i]['class'] == df_data[i-1]['class'] == df_data[i+1]['class']:
+
+            CKO = random.randint(95, 105) / 100
+
+            red_correlation = CKO * (df_data[i-1]['red_correlation'] + df_data[i]['red_correlation'] + df_data[i+1]['red_correlation'])/3
+            red_contrast = CKO * (df_data[i - 1]['red_contrast'] + df_data[i]['red_contrast'] + df_data[i + 1]['red_contrast']) / 3
+            red_homogeneity = CKO * (df_data[i - 1]['red_homogeneity'] + df_data[i]['red_homogeneity'] + df_data[i + 1]['red_homogeneity']) / 3
+            red_energy = CKO * (df_data[i - 1]['red_energy'] + df_data[i]['red_energy'] + df_data[i + 1]['red_energy']) / 3
+
+            green_correlation = CKO * (df_data[i - 1]['green_correlation'] + df_data[i]['green_correlation'] + df_data[i + 1]['green_correlation']) / 3
+            green_contrast = CKO * (df_data[i - 1]['green_contrast'] + df_data[i]['green_contrast'] + df_data[i + 1]['green_contrast']) / 3
+            green_homogeneity = CKO * (df_data[i - 1]['green_homogeneity'] + df_data[i]['green_homogeneity'] + df_data[i + 1]['green_homogeneity']) / 3
+            green_energy = CKO * (df_data[i - 1]['green_energy'] + df_data[i]['green_energy'] + df_data[i + 1]['green_energy']) / 3
+
+            blue_correlation = CKO * (df_data[i - 1]['blue_correlation'] + df_data[i]['blue_correlation'] + df_data[i + 1]['blue_correlation']) / 3
+            blue_contrast = CKO * (df_data[i - 1]['blue_contrast'] + df_data[i]['blue_contrast'] + df_data[i + 1]['blue_contrast']) / 3
+            blue_homogeneity = CKO * (df_data[i - 1]['blue_homogeneity'] + df_data[i]['blue_homogeneity'] + df_data[i + 1]['blue_homogeneity']) / 3
+            blue_energy = CKO * (df_data[i - 1]['blue_energy'] + df_data[i]['blue_energy'] + df_data[i + 1]['blue_energy']) / 3
+
+            cl = df_data[i]['class']
+
+            df_data_new.append({'red_correlation': red_correlation,
+                                'red_contrast': red_contrast,
+                                'red_homogeneity': red_homogeneity,
+                                'red_energy': red_energy,
+                                'green_correlation': green_correlation,
+                                'green_contrast': green_contrast,
+                                'green_homogeneity': green_homogeneity,
+                                'green_energy': green_energy,
+                                'blue_correlation': blue_correlation,
+                                'blue_contrast': blue_contrast,
+                                'blue_homogeneity': blue_homogeneity,
+                                'blue_energy': blue_energy,
+                                'class': cl
+                                })
+        else:
+            continue
+
+    return df_data_new + df_data
 
 
 if __name__ == "__main__":
-    # подготовка данных
-    Y = [1, 2, 3, 4, 5, 6, 7, 8]
-    # кодирование классов
-    encoder = LabelEncoder()
-    encoder.fit(Y)
-    encoded_Y = encoder.transform(Y)
-    dummy_y = np_utils.to_categorical(encoded_Y)
-
-    print("dummy_y[0]: ", dummy_y[0])
-
     ''''
     ill_to_index = {'Бурая ржавчина': 0,
                     'Желтая ржавчина': 1,
@@ -160,33 +172,67 @@ if __name__ == "__main__":
                     }
     '''
 
-    df_data = pd.DataFrame([[0., 0., 0., 0.]], [])  # saving parameters Haralick's
-    df_res = pd.DataFrame([[0., 0., 0., 0., 0., 0., 0., 0.]])  # saving labels
+    df_test = pd.DataFrame(generator_data('./test_dir/'))
+    df_val = pd.DataFrame(generator_data('./val_dir/'))
+    df_train = (generator_data('./train_dir/'))
 
-    image_index = 0
-    illness_index = 1
-    root = './test_dir/'
+    #
 
-    generator_data('./test_dir/')
-    generator_data('./val_dir/')
-    generator_data('./train_dir/')
+    #
+    #
+    # df_train = pd.DataFrame(generator_data('./train_dir/'))
+    # x_train = df_train.iloc[:, 0:12]
+    # y_train = df_train.iloc[:, 12]
+    #
 
-    print('df_data ', df_data.shape)
-    print(df_data)
+    df_train = pd.DataFrame(generate_haralick_params(df_train))
 
-    df_data = df_data.to_numpy()
-    df_res = df_res.to_numpy()
+    x_train = df_train.iloc[:, 0:12]
+    y_train = df_train.iloc[:, 12]
+    #
 
-    X_train, X_test, y_train, y_test = train_test_split(df_data, df_res, test_size=0.2, stratify=df_res, random_state=25)
+    # увеличиваем тестовый датасет
+    df_test = pd.concat([df_test, df_val])
 
-    model = Sequential()
-    model.add(Dense(4, input_dim=4, activation='relu'))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dropout(0.25))
-    model.add(Dense(8, activation='softmax'))
+    x_test = df_test.iloc[:, 0:12]
+    y_test = df_test.iloc[:, 12]
+    # # #
 
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=[metrics.CategoricalAccuracy()])
-    model.fit(X_train, y_train, epochs=200, batch_size=2, verbose=1, validation_split=0.1)
-    test_loss, test_acc = model.evaluate(X_test, y_test)
-    print('Acc: ', test_acc)
+
+    parameter_space = {
+        'alpha': [0.0001, 0.001, 0.01],
+        'hidden_layer_sizes': [(30, 30), (40, 40), (50, 50), (100, 100)],
+        'activation': ['relu', 'logistic'],
+        'solver': ['lbfgs'],
+        'max_iter': [100, 200, 300, 400, 500, 600, 700]
+    }
+    mlp = MLPClassifier(verbose=0)
+
+    clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3)
+    clf.fit(x_train, y_train)
+    print('Best parameters found:\n', clf.best_params_)
+
+    means = clf.cv_results_['mean_test_score']
+    print(max(means))
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+    #
+    clf = MLPClassifier(activation='logistic',
+                        alpha=0.01,
+                        hidden_layer_sizes=(30, 30),
+                        max_iter=1000,
+                        solver='lbfgs')
+    clf.fit(x_train, y_train)
+
+    print('Train size = {}, test size = {}'.format(len(y_train), len(y_test)))
+
+    accuracy = clf.score(x_test, y_test)
+    print("Accuracy = {}\n".format(accuracy))
+    target_names = ['1', '2', '3', '4', '5', '6', '7', '8']
+    y_pred = clf.predict(x_test)
+    print(classification_report(y_test, y_pred, target_names=target_names))
+    disp = metrics.plot_confusion_matrix(clf, x_test, y_test)
+    disp.figure_.suptitle("Confusion Matrix")
+    print("Confusion matrix:\n%s" % disp.confusion_matrix)
